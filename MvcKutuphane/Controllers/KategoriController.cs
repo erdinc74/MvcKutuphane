38ca28e2fc6a033443bf74bda88fe53be1b57879
HEGA <HEGA@DESKTOP-38BGNV5>
2020-07-06 18:44:40 +0300

    Add project files.

---
+++ MvcKutuphane/Controllers/KategoriController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+
+namespace MvcKutuphane.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORI.ToList();
+            return View(degerler);
+        }
+    }
+}
